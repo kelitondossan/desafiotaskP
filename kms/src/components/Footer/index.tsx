6915616156c01412a79dfedf69c9daaa8f0d72b4
keliton santos <71714438+kelitondossan@users.noreply.github.com>
2023-11-16 23:58:17 -0300

    Update index.tsx

--- kms/src/components/Footer/index.tsx
+++ kms/src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { FooterContainer } from "./styles";
 const Footer = () => {
   return (
     <FooterContainer>
-      <p>MKS sistemas © Todos os direitos reservados</p>
+      <p>MKS sistemas © Todos os direitos reservados DEV</p>
     </FooterContainer>
   );
 };
